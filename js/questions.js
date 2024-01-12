c272b4c5e515465a2aa6fe9843e4e5c663e689e6
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-01-12 17:27:35 +0000

    adding first 3 questions

---
+++ js/questions.js
@@ -0,0 +1,21 @@
+// questions for the quiz
+var question1 = {
+  question: "Inside which HTML element do we put the JavaScript?",
+  options: ["<script>", "<javascript>", "<scripting>", "<js>"],
+  answer: "<script>",
+};
+var question2 = {
+  question: "The external JavaScript file must contain the <script> tag.",
+  options: ["True", "False"],
+
+  answer: "False",
+};
+var question3 = {
+  question: "Where is the correct place to insert a JavaScript?",
+  options: [
+    "The <body> section",
+    "Both the <head> section and the <body> section",
+    "The <head> section",
+  ],
+  answer: "Both the <head> section and the <body> section are correct    ",
+};
