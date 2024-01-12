fec09358d938c56465db8e2f290494ce5041e219
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-01-12 22:06:40 +0000

    Initial code for first 3 questions to test logic of App.

--- js/questions.js
+++ js/questions.js
@@ -1,21 +1,66 @@
 // questions for the quiz
-var question1 = {
-  question: "Inside which HTML element do we put the JavaScript?",
-  options: ["<script>", "<javascript>", "<scripting>", "<js>"],
-  answer: "<script>",
-};
-var question2 = {
-  question: "The external JavaScript file must contain the <script> tag.",
-  options: ["True", "False"],
+const quizData = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    a: "<script>",
+    b: "<javascript>",
+    c: "<scripting>",
+    d: "<js>",
+    correct: "a",
+  },
+  {
+    question: "The external JavaScript file must contain the <script> tag.",
+    a: "True",
+    b: "False",
 
-  answer: "False",
-};
-var question3 = {
-  question: "Where is the correct place to insert a JavaScript?",
-  options: [
-    "The <body> section",
-    "Both the <head> section and the <body> section",
-    "The <head> section",
-  ],
-  answer: "Both the <head> section and the <body> section are correct    ",
-};
+    correct: "b",
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    a: "The <body> section",
+    b: "Both the <head> section and the <body> section",
+    c: "The <head> section",
+
+    correct: "b",
+  },
+];
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+var currentQuiz = 0;
+var score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+
+  console.log(currentQuizData.d);
+  //d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+  var answer;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+
+submitBtn.addEventListener("click", () => {
+  var answer = getSelected();
+  console.log(answer);
+});
