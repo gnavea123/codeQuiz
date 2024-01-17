b2460e9d9f9be8296898052d1dde0ab6e4182dc9
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-01-17 16:30:00 +0000

    added link to questions page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 //initial js script
+
+// function startQuiz() {
+//   windows.location =
+//     "https://gnavea123.github.io/codeQuiz/displayquestions.html";
+//   if (typeof window === "undefined") {
+//     console.log("Oops, `window` is not defined");
+//   }
+// }
+
+var questionUrl = "https://gnavea123.github.io/codeQuiz/displayquestions.html";
+function gotoLink() {
+  console.log(questionUrl);
+  window.open(questionUrl);
+}
